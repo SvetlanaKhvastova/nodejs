83b79a6cc4902f3cf0cfe7e1b54e95a2bab51063
Svetlana Khvastova <svetakhvastova@gmail.com>
2021-06-12 14:16:52 +0300

    authorization

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-
-const router = require("./routes/router");
+const { router } = require("./routes/routes");
 
 const PORT = process.env.PORT || 5001;
 
