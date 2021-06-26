a72f805707ab933c31783ce05a6ccdd639a264ec
Svetlana Khvastova <svetakhvastova@gmail.com>
2021-06-26 11:44:32 +0300

    lesson 4

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const multer = require("multer");
 require("dotenv").config();
-const { router } = require("./routes/routes");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 const app = express();
-app.use(express.json());
-app.use(router);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}---${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.json({ message: `HELLO` });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on the port ${PORT}`);
